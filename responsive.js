fc6efe5a734b9af22a434080ba48aea1938ed6aa
PrathnaChaudhary <chaudharyprathna@gmail.com>
2024-09-14 11:09:49 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,9 @@
+// JavaScript to handle scroll effect for the navigation bar
+window.addEventListener('scroll', function() {
+    const nav = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
